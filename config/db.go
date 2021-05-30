c6b2e7b931e31f5efa4aebc9203da24d73c57e74
kumneung <sing3demons@live.com>
2021-05-30 12:01:10 +0700

    add docker file

--- config/db.go
+++ config/db.go
@@ -2,18 +2,31 @@ package config
 
 import (
 	"app/models"
+	"fmt"
+	"os"
 
-	"gorm.io/driver/sqlite"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 var db *gorm.DB
 
 func InitDB() {
-	database, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	// database, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	USER := os.Getenv("DB_USER")
+	PASS := os.Getenv("DB_PASSWORD")
+	HOST := os.Getenv("DB_HOST")
+	DBNAME := os.Getenv("DB_NAME")
+
+	URL := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local", USER, PASS,
+		HOST, DBNAME)
+
+	database, err := gorm.Open(mysql.Open(URL))
+
 	if err != nil {
 		panic("failed to connect database")
 	}
+
 	database.AutoMigrate(&models.Product{})
 
 	db = database
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gofiber/fiber/v2 v2.5.0
 	github.com/jinzhu/copier v0.2.8 // indirect
 	github.com/joho/godotenv v1.3.0
+	gorm.io/driver/mysql v1.1.0
 	gorm.io/driver/sqlite v1.1.4
-	gorm.io/gorm v1.21.3
+	gorm.io/gorm v1.21.9
 )
